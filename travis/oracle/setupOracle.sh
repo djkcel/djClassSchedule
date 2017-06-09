c939b67a0e2ef9bebe5b4f2c844357c188983465
Christopher Davenport <ChristopherDavenport@outlook.com>
2017-06-09 11:28:51 -0400

    Strip Listed Migration to basename

--- travis/oracle/setupOracle.sh
+++ travis/oracle/setupOracle.sh
@@ -2,6 +2,6 @@
 
 for migration in $(ls ${TRAVIS_BUILD_DIR}/travis/oracle/*.sql | sort --version-sort)
 do
-    echo "Running Migration - ${migration}"
+    echo "Running Migration - $(basename ${migration})"
     sqlplus -L -S ${DB_USER}/${DB_PASS} @${migration}
 done
